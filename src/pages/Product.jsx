28320501fa2d62351f08c91de29571844ee0934f
Håkon Willand Engebretsen <willand27@hotmail.com>
2023-10-11 16:01:47 +0200

    add product page

---
+++ src/pages/Product.jsx
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+
+function Product() {
+  const { id } = useParams();
+  const [productData, setProductData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchProductData = async () => {
+      try {
+        const response = await fetch(
+          `https://api.noroff.dev/api/v1/online-shop/${id}`
+        );
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        setProductData(data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching product data:", error);
+        setLoading(false);
+      }
+    };
+
+    fetchProductData();
+  }, [id]);
+
+  return (
+    <div>
+      <h2>Product Details</h2>
+      {loading ? (
+        <p>Loading...</p>
+      ) : productData ? (
+        <div>
+          <h3>{productData.title}</h3>
+          <p>Price: ${productData.price}</p>
+          <p>Description: {productData.description}</p>
+          {/* Display other product details here */}
+        </div>
+      ) : (
+        <p>Product not found</p>
+      )}
+      <button onClick={() => navigate("/cart")}>Add to Cart</button>
+    </div>
+  );
+}
+
+export default Product;
