cd554630267363c70f74b2881e4f38b882ef6393
Håkon Willand Engebretsen <willand27@hotmail.com>
2023-10-12 12:36:25 +0200

    add tailwind and cart overlay

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,31 +1,43 @@
-import "./style.scss";
 import Logo from "../../assets/images/ecart-logo-color.png";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope, faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import { useCart } from "../../CartContext";
 
 const Header = () => {
-  const menuItems = [
-    { url: "/contact", text: "Contact", icon: faEnvelope },
-    { url: "/cart", text: "Cart", icon: faShoppingCart },
-  ];
+  const { state, dispatch } = useCart();
 
   return (
     <header>
       <nav className="flex justify-between">
         <Link to="/">
-          <img className="logo" src={Logo} alt="Logo" />
+          <img className="h-20" src={Logo} alt="Logo" />
         </Link>
 
-        <ul>
-          {menuItems.map((item) => (
-            <li key={item.text}>
-              <Link to={item.url}>
-                {item.text}
-                <FontAwesomeIcon className="menu-icon" icon={item.icon} />
-              </Link>
-            </li>
-          ))}
+        <ul className="flex gap-10 items-center">
+          <li>
+            <Link to="/contact">
+              <span className="pe-2">Contact</span>
+              <span className="p-2 bg-brand-orange rounded-full">
+                <FontAwesomeIcon icon={faEnvelope} />
+              </span>
+            </Link>
+          </li>
+
+          <li className="flex gap-x-2">
+            <Link to="/cart">
+              {state.totalItems === 0 ? (
+                <span className="pe-2">Cart</span>
+              ) : state.totalItems === 1 ? (
+                <span className="pe-2">{state.totalItems} item</span>
+              ) : (
+                <span className="pe-2">{state.totalItems} items</span>
+              )}
+              <span className="p-2 bg-brand-orange rounded-full">
+                <FontAwesomeIcon icon={faShoppingCart} />
+              </span>
+            </Link>
+          </li>
         </ul>
       </nav>
     </header>
