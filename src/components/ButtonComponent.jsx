f2da29b37885d8e0cbdb07ba246e20f608ce1c5c
Håkon Willand Engebretsen <willand27@hotmail.com>
2023-10-15 16:01:46 +0200

    button style

---
+++ src/components/ButtonComponent.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import styled from "styled-components";
+
+const BaseButton = styled.button`
+  background-color: #f9a828;
+  border-radius: 6px;
+  padding: 10px 20px;
+  cursor: pointer;
+  transition: background-color linear 800ms;
+  margin-top: 20px;
+  :hover {
+    background-color: #2e383f;
+    color: white;
+  }
+`;
+
+const PrimaryButton = styled(BaseButton)`
+  background-color: #2e383f;
+  color: white;
+  :hover {
+    background-color: #2c617d;
+    color: white;
+  }
+`;
+
+function Button() {
+  return (
+    <div>
+      <BaseButton>Base button</BaseButton>
+      <PrimaryButton>Primary button</PrimaryButton>
+    </div>
+  );
+}
+
+export { Button, BaseButton, PrimaryButton };
