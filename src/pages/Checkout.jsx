39840273ef0c0014e1f8b3567bf437436c5fde1f
Håkon Willand Engebretsen <willand27@hotmail.com>
2023-10-15 21:13:07 +0200

    add checkout content

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,5 +1,32 @@
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { BaseButton } from "../components/ButtonComponent";
+
 function Checkout() {
-  return <div>Hello</div>;
+  useEffect(() => {
+    const hasRefreshed = localStorage.getItem("hasRefreshed");
+
+    if (!hasRefreshed) {
+      localStorage.setItem("hasRefreshed", "true");
+      window.location.reload();
+    }
+  }, []);
+
+  return (
+    <div className="flex justify-center">
+      <div className="flex flex-col">
+        <h1 className="text-2xl md:text-5xl my-4 md:my-8 self-center">
+          Thank you for shopping at E-Cart!
+        </h1>
+        <span className="self-center">
+          A confirmation has been sent to your e-mail.
+        </span>
+        <Link className="self-center" to="/">
+          <BaseButton>Home</BaseButton>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Checkout;
