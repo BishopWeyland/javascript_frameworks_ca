54fdb0b7ebb773be9f7bd0a404aeceec25c8229e
Håkon Willand Engebretsen <willand27@hotmail.com>
2023-10-15 21:14:49 +0200

    fix cart small screen and style

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,15 +1,16 @@
+import React, { useContext } from "react";
 import Logo from "../../assets/images/ecart-logo-color.png";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope, faShoppingCart } from "@fortawesome/free-solid-svg-icons";
-import { useCart } from "../../CartContext";
+import { CartContext } from "../../CartContext";
 
 const Header = () => {
-  const { state, dispatch } = useCart();
+  const { state } = useContext(CartContext);
 
   return (
     <header>
-      <nav className="flex justify-between">
+      <nav className="flex justify-between mb-5">
         <Link to="/">
           <img className="h-20" src={Logo} alt="Logo" />
         </Link>
@@ -17,24 +18,38 @@ const Header = () => {
         <ul className="flex gap-10 items-center">
           <li>
             <Link to="/contact">
-              <span className="pe-2">Contact</span>
-              <span className="p-2 bg-brand-orange rounded-full">
-                <FontAwesomeIcon icon={faEnvelope} />
+              <span className="pe-2">
+                <span className="me-3 hidden sm:inline hover:border-b-2 hover:border-black">
+                  Contact
+                </span>
+                <span className="p-2 bg-brand-orange rounded-full">
+                  <FontAwesomeIcon icon={faEnvelope} />
+                </span>
               </span>
             </Link>
           </li>
 
           <li className="flex gap-x-2">
             <Link to="/cart">
-              {state.totalItems === 0 ? (
-                <span className="pe-2">Cart</span>
-              ) : state.totalItems === 1 ? (
-                <span className="pe-2">{state.totalItems} item</span>
-              ) : (
-                <span className="pe-2">{state.totalItems} items</span>
-              )}
-              <span className="p-2 bg-brand-orange rounded-full">
-                <FontAwesomeIcon icon={faShoppingCart} />
+              <span className="pe-2 ">
+                {state.totalItems !== null && !isNaN(state.totalItems) ? (
+                  <span className="me-3 hidden sm:inline hover:border-b-2 hover:border-black">
+                    {state.totalItems === 0
+                      ? "Cart"
+                      : state.totalItems === 1
+                      ? `${state.totalItems} item`
+                      : `${state.totalItems} items`}
+                  </span>
+                ) : (
+                  <span className="me-3 hidden sm:inline">Cart</span>
+                )}
+                <span className="p-2 bg-brand-orange rounded-full">
+                  <FontAwesomeIcon
+                    icon={faShoppingCart}
+                    className="mr-2 md:mr-0"
+                  />
+                  <span className="sm:hidden">{state.totalItems}</span>
+                </span>
               </span>
             </Link>
           </li>
