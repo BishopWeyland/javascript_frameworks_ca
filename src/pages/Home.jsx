67cb5b8512f221dc47695790f396ccc17256193e
Håkon Willand Engebretsen <willand27@hotmail.com>
2023-10-15 21:14:06 +0200

    add hero and style

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from "react";
 import { fetchProducts } from "../Api";
 import ProductCard from "../components/ProductCard";
 import { default as Search } from "../components/Search";
+import { PrimaryButton } from "../components/ButtonComponent";
+import { Element, scroller } from "react-scroll";
+import Hero from "../assets/images/ervo-rocks-Zam8TvEgN5o-unsplash.jpg";
 
 function Home() {
   const [data, setData] = useState([]);
@@ -24,10 +27,43 @@ function Home() {
     fetchData();
   }, []);
 
+  const scrollToPopularProducts = () => {
+    scroller.scrollTo("PopularProducts", {
+      duration: 800,
+      smooth: "easeInOutQuart",
+    });
+  };
+
   return (
-    <div className="px-5 py-12">
-      <div className="flex items-center justify-between my-10">
-        <h1 className="text-3xl font-bold font-serif">Popular products:</h1>
+    <div>
+      <div
+        className="h-max py-56 px-10 flex justify-center md:h-screen bg-cover bg-center rounded-sm"
+        style={{ backgroundImage: `url(${Hero})` }}
+      >
+        <div className="flex flex-col">
+          <h1 className="font-bold self-center text-center my-4  text-2xl md:text-5xl md:my-8">
+            Headsets for your comfort and convenience
+          </h1>
+          <p className="self-center text-center font-bold">
+            At E-Cart you can get up to 50% off on selected headsets this
+            weekend!
+          </p>
+          <PrimaryButton
+            className="self-center w-72 md:w-96"
+            onClick={scrollToPopularProducts}
+          >
+            Browse
+          </PrimaryButton>
+        </div>
+      </div>
+
+      <div className="flex flex-col justify-center md:flex-row items-center md:justify-between my-10">
+        {" "}
+        <Element name="PopularProducts">
+          <h2 className="text-2xl md:text-5xl my-4 md:my-8">
+            Popular products:
+          </h2>
+        </Element>
         <div className="w-72">
           <Search
             searchInput={searchInput}
@@ -41,12 +77,16 @@ function Home() {
       {loading ? (
         <p>Loading...</p>
       ) : (
-        <div className="flex flex-wrap gap-10 justify-center items-center">
-          {searchInput
-            ? filteredProducts.map((item) => (
-                <ProductCard key={item.id} product={item} />
-              ))
-            : data.map((item) => <ProductCard key={item.id} product={item} />)}
+        <div>
+          <div className="flex flex-wrap gap-10 justify-center items-center">
+            {searchInput
+              ? filteredProducts.map((item) => (
+                  <ProductCard key={item.id} product={item} />
+                ))
+              : data.map((item) => (
+                  <ProductCard key={item.id} product={item} />
+                ))}
+          </div>
         </div>
       )}
     </div>
